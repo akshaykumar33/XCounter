e1e987ca53d150a5934f6f5f76b8e847f9b87a77
Akshaykumar <akshaykumarpatil33@gmail.com>
2024-03-12 20:58:03 +0530

    counter full project commit

---
+++ xcounter/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <h1>Counter App </h1>
+        <h2>Count: {count}</h2>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrement
+        </button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
